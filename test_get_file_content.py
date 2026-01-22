2d7aeca1f3a77a2f657f284a88531b172ca5dcd2
vassilispap <vassilis.pap@gmail.com>
2026-01-22 19:31:42 +0200

    Lesson 2-3

---
+++ test_get_file_content.py
@@ -0,0 +1,31 @@
+from functions.get_file_content import get_file_content
+from config import MAX_CHARS
+
+import os
+
+def test():
+
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for lorem.txt")
+    print(result[MAX_CHARS:])
+    print("")
+
+    result = get_file_content("calculator", "main.py")
+    print("Result for main.py")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for pkg/calculator.py")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for /bin/cat")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for pkg/does_not_exist.py")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
