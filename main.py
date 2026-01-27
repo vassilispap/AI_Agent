da95f93bf6af78fe2b1053b4d5421b027c1f6ece
vassilispap <vassilis.pap@gmail.com>
2026-01-28 00:04:25 +0200

    Lesson 4-1

--- main.py
+++ main.py
@@ -18,45 +18,63 @@ def main():
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    
-    response = client.models.generate_content(    
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        )
-    )
-    if not response.usage_metadata:
-        raise RuntimeError("Gemini API response appears to be malformed")
-
-    if args.verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-
-    if not response.usage_metadata:
-        raise RuntimeError("Gemini API response appears to be malformed")
-
-    if args.verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-
-    if not response.function_calls:
-        print("Response:")
-        print(response.text)
-        return
-
     function_responses = []
-    for function_call in response.function_calls:
-        result = call_function(function_call, args.verbose)
-        if (
-            not result.parts
-            or not result.parts[0].function_response
-            or not result.parts[0].function_response.response
-        ):
-            raise RuntimeError(f"Empty function response for {function_call.name}")
+
+    for _ in range(20):
+        response = client.models.generate_content(    
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            )
+        )
+
+        if response.candidates[0]:
+            for candidate in response.candidates:
+                messages.append(response.candidates[0].content)
+
+        if not response.usage_metadata:
+            raise RuntimeError("Gemini API response appears to be malformed")
+
         if args.verbose:
-            print(f"-> {result.parts[0].function_response.response}")
-        function_responses.append(result.parts[0])
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+        if not response.usage_metadata:
+            raise RuntimeError("Gemini API response appears to be malformed")
+
+        if args.verbose:
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+        if not response.function_calls:
+            print("Response:")
+            print(response.text)
+            return
+
+        for function_call in response.function_calls:
+            result = call_function(function_call, args.verbose)
+            if (
+                not result.parts
+                or not result.parts[0].function_response
+                or not result.parts[0].function_response.response
+            ):
+                raise RuntimeError(f"Empty function response for {function_call.name}")
+            if args.verbose:
+                print(f"-> {result.parts[0].function_response.response}")
+            function_responses.append(result.parts[0])
+        messages.append(
+            types.Content(
+                role="user",
+                parts=function_responses,
+            )
+        )
+
+        if _ == 19:
+            print(f"{_ + 1} steps was reached, which is the maximum defined. No more function calls possible")
+            exit(1)
+
+
 
 
 
